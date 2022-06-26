acb278ae416fdd8fe0f5611dbf5e068bee491e5b
inkfarer <inkfarer@inkfarer.com>
2022-06-26 12:18:54 +0300

    Tweak appearance of commentator display over gameplay

--- src/graphics/pages/gameplay/components/Casters.vue
+++ src/graphics/pages/gameplay/components/Casters.vue
@@ -13,16 +13,13 @@
             </div>
             <div class="casters-content">
                 <div
-                    v-for="(caster, id, index) in casters.data ?? {}"
+                    v-for="(caster, id) in casters.data ?? {}"
                     :key="id"
                     class="caster-item"
                 >
                     <fitted-content :max-width="264">
                         <span class="caster-name">{{ caster.name }}</span>
-                        <badge
-                            class="caster-pronouns"
-                            :color="index % 2 === 0 ? COLORS.accent1 : COLORS.accent2"
-                        >
+                        <badge class="caster-pronouns">
                             {{ caster.pronouns }}
                         </badge>
                     </fitted-content>
@@ -43,7 +40,6 @@ import { Casters } from 'schemas';
 import { DASHBOARD_BUNDLE_NAME } from '../../../../shared/constants';
 import FittedContent from '../../../components/FittedContent.vue';
 import Badge from '../../../components/Badge.vue';
-import { COLORS } from '../../../styles/colors';
 import { ref } from 'vue';
 import gsap from 'gsap';
 
@@ -66,7 +62,6 @@ export default defineComponent({
 
         return {
             casters,
-            COLORS,
             castersVisible,
 
             castersEnter: (elem: HTMLElement, done: gsap.Callback) => {
@@ -91,24 +86,24 @@ export default defineComponent({
     bottom: 30px;
     right: 60px;
     width: 280px;
+    overflow: hidden;
+    border-radius: $default-border-radius;
 
-    filter: drop-shadow(-2px 2px 4px $drop-shadow-color);
+    filter: drop-shadow(0 0 4px $drop-shadow-color);
 
     > .casters-header {
         font-size: 28px;
         text-align: center;
         width: 100%;
-        background-color: $text-background-secondary-alpha;
-        border-radius: $default-border-radius $default-border-radius 0 0;
+        background-color: $text-background;
         padding: 4px 0;
-        filter: drop-shadow(0 2px 5px rgba(34, 34, 34, 0.25));
+        border-bottom: 2px solid $accent-yellow;
     }
 
     > .casters-content {
         min-height: 100px;
         padding: 8px;
-        background-color: $text-background-alpha;
-        border-radius: 0 0 $default-border-radius $default-border-radius;
+        @include striped-background();
         display: flex;
         flex-direction: column;
         justify-content: center;
